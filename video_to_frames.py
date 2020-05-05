09155c8965c0a4d25f19180cd43ed2b175c20283
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-05 13:28:27 -0400

    update0505

--- video_to_frames.py
+++ video_to_frames.py
@@ -2,12 +2,11 @@ import cv2
 
 def read_frames(filepath):
     video = cv2.VideoCapture(filepath)
+    print("Video successfully opened.")
     count = 0
     is_reading = 1
 
     while is_reading:
         is_reading, img = video.read()
-        cv2.imwrite("frame_%d.jpg" % count, img)
+        cv2.imwrite("frames/fr%d.png" % count, img)
         count += 1
-
-    pass
