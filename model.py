478a6c5902889891ae74e5dd43adecf2c9a63d06
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-05 18:55:41 -0400

    look this

--- model.py
+++ model.py
@@ -1,22 +1,23 @@
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.applications import vgg19, VGG19
+from tensorflow.keras.applications import VGG19
 
 class Model():
     def __init__(self):
-        super().__init__()
+        # TODO: initialize all self attributes
+        self.learning_rate = 0.1
     
-    # Load the VGG19 network with pre-trained weights.
-    model = VGG19(include_top=False, weights='imagenet')
-    model.trainable = False
-    print("VGG19 model successfully loaded.")
+    def load(self):
+        model = VGG19(include_top=False, weights='imagenet')
+        print("VGG19 model successfully loaded.")
+        self.model = model
+        self.layer_outputs = dict([(layer.name, layer.output) for layer in model.layers])
 
-    # LOSS: This section contains the loss function and four helper functions.
-    def _content_loss(img, content):
+    # This section contains the loss function and four helper functions.
+    def _content_loss(self, img, content):
         return tf.math.reduce_sum(tf.math.square(img - content))
 
-    def _gram_matrix(img, area, num_channels):
+    def _gram_matrix(self, img, area, num_channels):
         """
         Compute the gram matrix G for an image tensor
 
@@ -28,7 +29,7 @@ class Model():
         gram = tf.matmul(tf.transpose(mat), mat)
         return gram
 
-    def _layer_style_loss(img, style):
+    def _layer_style_loss(self, img, style):
         """
         Compute the style loss in a single layer
 
@@ -44,7 +45,7 @@ class Model():
         loss = tf.math.reduce_sum(tf.math.square(gram_img - gram_style)) / (area * num_channels * 2)**2
         return loss
 
-    def _style_loss(map_set):
+    def _style_loss(self, map_set):
         """
         Compute the total style loss across all layers
 
@@ -62,7 +63,7 @@ class Model():
 
         return sum(layer_losses)
 
-    def loss(img, content, style, alpha=10, beta=80):
+    def loss(self, img, content, style, alpha=10, beta=80):
         l_content = _content_loss(img, content)
         l_style = _style_loss(img, style)
         loss = alpha * l_content + beta * l_style
