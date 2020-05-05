3df78c6ec7482da7ff0501070ab1ac07daeb3d27
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-04 23:04:33 -0400

    update0504

--- model.py
+++ model.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
-from tensorflow.keras.applications import VGG19
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications import vgg19, VGG19
 from loss_functions import total_loss
+
+def load_img(style_filepath, content_filepath):
+    """
+    Load the preprocessed style image and content image
+    """
+    style = plt.imread(style_filepath)
+    content = plt.imread(content_filepath)
+    # Preprocess the images to satisfy the model requirement
+    style = vgg19.preprocess_input(style)
+    content = vgg19.preprocess_input(content)
+    return style, content
+
 
 def load_vgg19():
     """
