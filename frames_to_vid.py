6632bd7452ca19518b5f8cc4b94bb24bdf577741
byu14 <brandonyu1215@gmail.com>
2020-05-07 16:03:15 +0800

    added print statements

--- frames_to_vid.py
+++ frames_to_vid.py
@@ -10,29 +10,32 @@ def frames_to_vid(pathIn, pathOut, fps):
 
     # sorting the file names according to * in frame_*.png
     files.sort(key = lambda x: int(x[6:-4]))
+    n = len(files)
 
-    for i in range(len(files)):
+    print('Reading frames ...')
+    for i in range(n):
         filename = pathIn + files[i]
         #reading each files
         img = cv2.imread(filename)
         height, width, layers = img.shape
-        size = (width,height)
+        size = (width, height)
         # print(filename)
         #inserting the frames into an image array
         frame_array.append(img)
-    print('Successfully read all files.')
+    print('Successfully read %d frames.' % n)
 
-    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'mp4v'), fps, size) # alternatively *'XVID' with .avi
+    out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'mp4v'), fps, size) # alternatively *'XVID' with .avi
 
+    print('Processing video ...')
     for i in range(len(frame_array)):
         # writing to a image array
         out.write(frame_array[i])
     out.release()
-    print('Output video saved.')
+    print('Output video saved. Video legnth: %f seconds.' % (len(frame_array) / fps))
 
 
 # def main():
-#     frames_to_vid('./frames/', 'style_video.mp4', 15) # fps would prob be 30
+#     frames_to_vid('./frames/', 'style_video.mp4', 25) # fps would prob be 30
 
 # if __name__ == "__main__":
 #     main()
