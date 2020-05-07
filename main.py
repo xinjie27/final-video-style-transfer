09a58a8cea624cca8f73ca60c52c38c13d231c9a
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-07 11:01:31 -0400

    a

--- main.py
+++ main.py
@@ -2,23 +2,46 @@ import argparse
 import numpy as np
 from utils import *
 from model import Model, Evaluator
+import time
+from scipy.optimize import fmin_l_bfgs_b
+from tensorflow.keras.preprocessing.image import save_img
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--video', required=True, help='path to the video')
+    parser.add_argument('-v', '--video', required=True, help='path to the video')
+    parser.add_argument('-s', '--style_img', required=True, help='path to the style image')
     parser.add_argument('-o', '--output', required=True, help='output path')
     parser.add_argument('--width', required=False, help='output image width')
     parser.add_argument('--height', required=False, help='output image height')
     parser.add_argument('--iter', required=False, help='hyperparameter: number of training iterations')
 
     args = parser.parse_args()
-    input_path = args.video
+    video_path = args.video
+    style_path = args.style_img
     output_path = args.output
     # Parse optional arguments
     img_height = args.height
     img_width = args.width
+    n_iters = args.iter
 
-    read_frames(input_path)
+    # read_frames(video_path)
+    model = Model(video_path, style_path)
+    model.gen_input()
+    evaluator = Evaluator(model)
+
+    for i in range(300):
+        print('Start of iteration', i)
+        start_time = time.time()
+        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
+                                        fprime=evaluator.grads, maxfun=20)
+        print('Current loss value:', min_val)
+        # save current generated image
+        img = model.deprocess_img(x.copy())
+        fname = '_at_iteration_%d.png' % i
+        save_img(fname, img)
+        end_time = time.time()
+        print('Image saved as', fname)
+        print('Iteration %d completed in %ds' % (i, end_time - start_time))
 
 if __name__ == "__main__":
     main()
