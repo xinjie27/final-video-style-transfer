bcb6b46693cc51c86da128190a9e6525db0996ee
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-07 10:09:38 -0400

    a

---
+++ main.py
@@ -0,0 +1,10 @@
+import argparse
+import numpy as np
+from utils import *
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--video', required=True, help='Path to the video')
+
+if __name__ == "__main__":
+    main()
