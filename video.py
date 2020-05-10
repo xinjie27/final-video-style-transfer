1cf21d4686d447ba840651956a47d3029cdbab66
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-09 21:01:16 -0400

    final video.py

--- video.py
+++ video.py
@@ -2,50 +2,57 @@ import cv2
 import os
 import numpy as np
 from os.path import isfile, join
+from model import Model
 
-def stylize_frame():
-    pass
+class Video(object):
+    def __init__(self, video_path, style_path, img_h, img_w, lr, n_iters):
+        self.video_path = video_path
+        self.style_path = style_path
+        self.img_height = img_h
+        self.img_width = img_w
+        self.lr = lr
+        self.n_iters = n_iters
+    
+    def _stylize_frame(self, img):
+        model = Model(img, self.style_path, self.img_height, self.img_width, self.lr)
+        model.optimize()
+        model.train(self.n_iters)
 
-def vid_to_frames(filepath):
-    video = cv2.VideoCapture(filepath)
-    print("Video successfully opened.")
-    count = 0  # Frame count
-    is_reading = 1
+    def vid_to_frames(self):
+        video = cv2.VideoCapture(self.filepath)
+        print("Video successfully opened.")
+        count = 0 # Frame count
+        is_reading = 1
 
-    while is_reading:
-        is_reading, img = video.read()
-        if (is_reading == False):
-            break
-        cv2.imwrite("frames/frame_%d.png" % count, img)
-        count += 1
-    print("All frames are successfully stylized.")
+        while is_reading:
+            is_reading, img = video.read()
+            if (is_reading == False):
+                break
+            _stylize_frame(img)
+            count += 1
+        print("All frames are successfully stylized.")
 
-def frames_to_vid(path_in, path_out, fps=30):
-    frame_array = []
-    files = [f for f in os.listdir(path_in) if isfile(join(path_in, f)) if not f.startswith('.')]
+    def frames_to_vid(self, path_in, path_out, fps=30):
+        frame_array = []
+        files = [f for f in os.listdir(path_in) if isfile(join(path_in, f)) if not f.startswith('.')]
 
-    # Sort the file names according to * in frame_*.png
-    files.sort(key = lambda x: int(x[6:-4]))
+        # Sort the file names according to * in frame_*.png
+        files.sort(key = lambda x: int(x[6:-4]))
 
-    for i in range(len(files)):
-        filename = path_in + files[i]
-        # Read each file
-        img = cv2.imread(filename)
-        height, width, layers = img.shape
-        size = (width,height)
-        # Insert the frames into an image array
-        frame_array.append(img)
-    print("Successfully read all files.")
+        for i in range(len(files)):
+            filename = path_in + files[i]
+            # Read each file
+            img = cv2.imread(filename)
+            height, width, layers = img.shape
+            size = (width,height)
+            # Insert the frames into an image array
+            frame_array.append(img)
+        print("Successfully read all files.")
 
-    out = cv2.VideoWriter(path_out, cv2.VideoWriter_fourcc(*'mp4v'), fps, size) # Alternatively *'XVID' with .avi
+        out = cv2.VideoWriter(path_out, cv2.VideoWriter_fourcc(*'mp4v'), fps, size) # Alternatively *'XVID' with .avi
 
-    for i in range(len(frame_array)):
-        # Write to a image array
-        out.write(frame_array[i])
-    out.release()
-    print("Stylized video saved.")
-
-def gen_noise_image(content, noise_ratio=0.6):
-    noise = np.random.uniform(-20., 20., content.shape).astype(np.float32)
-    img = noise_ratio * noise + (1 - noise_ratio) * content
-    return img
+        for i in range(len(frame_array)):
+            # Write to a image array
+            out.write(frame_array[i])
+        out.release()
+        print("Stylized video saved.")
