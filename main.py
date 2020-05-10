b0aea05abdc871ddb4bdbdbff560ce4cfd55ed12
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-09 20:00:43 -0400

    main.py updated

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse
 import numpy as np
-from utils import *
-from model import Model, Evaluator
 import time
 from scipy.optimize import fmin_l_bfgs_b
-from tensorflow.keras.preprocessing.image import save_img
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import save_img
+from model import Model, Evaluator
+from utils import *
 
 def main():
     tf.compat.v1.disable_v2_behavior()
@@ -14,6 +14,7 @@ def main():
     parser.add_argument('-v', '--video', required=True, help='path to the video')
     parser.add_argument('-s', '--style_img', required=True, help='path to the style image')
     parser.add_argument('-o', '--output', required=True, help='output path')
+    parser.add_argument('--fps', required=False, help='frames per second')
     parser.add_argument('--width', required=False, help='output image width')
     parser.add_argument('--height', required=False, help='output image height')
     parser.add_argument('--iter', required=False, help='hyperparameter: number of training iterations')
@@ -23,28 +24,13 @@ def main():
     style_path = args.style_img
     output_path = args.output
     # Parse optional arguments
+    fps = args.fps
     img_height = args.height
     img_width = args.width
     n_iters = args.iter
 
-    # read_frames(video_path)
-    model = Model(video_path, style_path)
-    evaluator = Evaluator(model)
-    x = get_noise_image(model.content)
-
-    for i in range(300):
-        print('Start of iteration', i)
-        start_time = time.time()
-        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
-                                        fprime=evaluator.grads, maxfun=20)
-        print('Current loss value:', min_val)
-        # save current generated image
-        img = model.deprocess_img(x.copy())
-        fname = '_at_iteration_%d.png' % i
-        save_img(fname, img)
-        end_time = time.time()
-        print('Image saved as', fname)
-        print('Iteration %d completed in %ds' % (i, end_time - start_time))
+    vid_to_frames(video_path)
+    frames_to_vid('./frames/', output_path, fps)
 
 if __name__ == "__main__":
     main()
