d2c184dd16b8bad39cf69fd3d7ee698dece597b1
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-11 10:46:56 -0400

    NEW PUSH

--- model.py
+++ model.py
@@ -1,80 +1,60 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-from tensorflow.keras.applications import vgg19, VGG19
-from tensorflow.keras.preprocessing.image import load_img, img_to_array, save_img
-from tensorflow.compat.v1 import variable_scope, get_variable, Session, global_variables_initializer, train, disable_eager_execution
-from tensorflow.keras import backend as K
-from tensorflow.keras import optimizers
-import argparse
 import os
-from scipy.optimize import fmin_l_bfgs_b
+import numpy as np
+import tensorflow as tf
 
-class Model(object):
-    def __init__(self, content, style_filepath, img_h, img_w, lr, frame_idx):
-        self.alpha = 1e-4
-        self.beta = 1
+from vgg19 import VGG
+from utils import *
+from os.path import isfile, join
+import argparse
+
+from tensorflow.compat.v1 import variable_scope, get_variable, Session, global_variables_initializer, train, disable_eager_execution
+tf.compat.v1.disable_eager_execution()
+
+class Image(object):
+    def __init__(self, content_filepath, style_filepath, img_h, img_w, lr, frame_idx):
         self.img_height = img_h
         self.img_width = img_w
-        # Layers in which we compute the style loss
-        self.style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
-        # Layer in which we compute the content loss
-        self.content_layer = 'block5_conv2'
-        # Global step and learning rate
-        self.gstep = tf.Variable(0, dtype=tf.int32, trainable=False, name="global_step")
-        self.lr = lr
-        self.frame_idx = frame_idx
-        self.opt = optimizers.Adam(self.lr)
-
-        self.gen_input()
-        self.load(content, style_filepath)
-        self.loss()
-    
-    def _gen_noise_image(self, content, noise_ratio=0.6):
-        noise = np.random.uniform(-20., 20., content.shape).astype(np.float32)
-        img = noise_ratio * noise + (1 - noise_ratio) * content
-        self.initial_img = img
-    
-    def _deprocess_img(self, img):
-        img = img.reshape((self.img_height, self.img_width, 3))
         
-        # Inverse mean-centering
-        img[:, :, 0] += 123.68
-        img[:, :, 1] += 116.779
-        img[:, :, 2] += 103.939
+        self.content_img = get_resized_image(content_filepath, img_width, img_height)
+        self.style_img = get_resized_image(style_filepath, img_width, img_height)
+        self.initial_img = generate_noise_image(self.content_img, img_width, img_height)
+        # self.prev_frame = get_resized_image(prev_img, img_width, img_height)
+        self.frame_idx = frame_idx
 
-        img = img[:, :, ::-1] # BGR -> RGB
-        img = np.clip(img, 0, 255).astype('uint8')
-        return img
-    
-    def load(self, content, style_filepath):
-        self.content = vgg19.preprocess_input(content)
+        # Layers in which we compute the content/style loss
+        self.content_layer = "block5_conv2"
+        self.style_layers = ["block1_conv1", "block2_conv1", "block3_conv1", "block4_conv1", "block5_conv1"]
 
-        style = load_img(style_filepath, target_size=(self.img_height, self.img_width))
-        style = img_to_array(style)
-        style = np.expand_dims(style, 0)
-        self.style = vgg19.preprocess_input(style)
-        content_img = K.variable(self.content)
-        style_img = K.variable(self.style)
+        # Hyperparameters alpha and beta
+        self.content_w = 1e-4
+        self.style_w = 1
 
-        self._gen_noise_image(self.content)
-        self.initial_img = K.variable(self.initial_img)
+        # Initialize the weights for all style layers; this hyperparameter can be tuned
+        # General idea: deeper layers are more important
+        self.style_layer_w = [0.5 * i + 0.5 for i in range(5)]
 
-        # Combine 3 images into a single tensor
-        tensor = K.concatenate([content_img, style_img, self.initial_img], axis=0)
-        self.model = VGG19(input_tensor=tensor, include_top=False, weights='imagenet')
-        print("VGG19 successfully loaded.")
-        self.layer_outputs = dict([(layer.name, layer.output) for layer in self.model.layers])
+        self.lr = lr
+        self.gstep = tf.Variable(0, dtype=tf.int32, trainable=False, name="global_step")  # global step
 
-    
-    def gen_input(self):
-        with variable_scope("input"):
-            self.input = get_variable("input", shape=([1, self.img_height, self.img_width, 3]), dtype=tf.float32, initializer=tf.zeros_initializer())
+
+    def create_input(self):
+        """
+        Initialize input tensor
+        """
+        with tf.compat.v1.variable_scope("input", reuse=tf.AUTO_REUSE):
+            self.input_img = tf.compat.v1.get_variable("in_img", shape=([1, self.img_height, self.img_width, 3]), dtype=tf.float32, initializer=tf.zeros_initializer())
+
+    def load_vgg(self):
+        self.vgg = VGG(self.input_img)
+        self.vgg.load()
+        # mean-centering
+        self.content_img -= self.vgg.mean_pixels
+        self.style_img -= self.vgg.mean_pixels
 
     # This section contains the loss function and four helper functions.
     def _content_loss(self, img, content):
-        return tf.math.reduce_sum(tf.math.square(img - content))
-
+        self.content_loss = tf.reduce_sum(tf.square(img - content))
+        
     def _gram_matrix(self, img, area, num_channels):
         """
         Compute the gram matrix G for an image tensor
@@ -86,129 +66,94 @@ class Model(object):
         gram = tf.matmul(tf.transpose(mat), mat)
         return gram
 
-    def _layer_style_loss(self, img, style):
+    def _layer_style_loss(self, style, img):
         """
         Compute the style loss in a single layer
         :param img: the input image
         :param style: the style image
         """
-        area = img.shape[0].value * img.shape[1].value
-        num_channels = img.shape[2].value
+        num_channels = style.shape[3]
+        area = style.shape[1] * style.shape[2]
 
         gram_style = self._gram_matrix(style, area, num_channels)
         gram_img = self._gram_matrix(img, area, num_channels)
 
-        loss = tf.math.reduce_sum(tf.math.square(gram_img - gram_style)) / (area * area * num_channels * num_channels * 4)
-        return loss
+        return tf.reduce_sum(tf.square(gram_img - gram_style)) / ((2 * area * num_channels) ** 2)
 
-    def _style_loss(self, combination_maps, style_maps):
+    def _style_loss(self, style_maps):
         """
         Compute the total style loss across all layers
-        :param combination_maps: a set of all feature maps for the combination image
+
         :param style_maps: a set of all feature maps for the style image
         """
-        num_layers = style_maps.shape[0]
+        # We use self.style_layers specified above to compute the total style loss
+        num_layers = len(style_maps) # should be 5
 
-        # Initialize the weights for all style layers; this hyperparameter can be tuned
-        # General idea: deeper layers are more important
-        layer_weights = [0.5 * i + 0.5 for i in range(num_layers)]
+        unweighted_loss = [self._layer_style_loss(style_maps[i], getattr(self.vgg, self.style_layers[i]))
+             for i in range(num_layers)]
 
-        layer_losses = []
-        for i in range(num_layers):
-            layer_loss = self._layer_style_loss(combination_maps[i], style_maps[i]) * layer_weights[i]
-            layer_losses.append(layer_loss)
-
-        return sum(layer_losses)
+        self.style_loss = sum(self.style_layer_w[i] * unweighted_loss[i] for i in range(num_layers))
 
     def loss(self):
         """
         Compute the total loss of the model
         """
-        # Content loss
-        layer_features = self.layer_outputs[self.content_layer]
-        content_out = layer_features[0, :, :, :]
-        combination_out = layer_features[2, :, :, :]
+        with tf.compat.v1.variable_scope("loss", reuse=tf.AUTO_REUSE):
+            # Content loss
+            with tf.compat.v1.Session() as sess:
+                sess.run(self.input_img.assign(self.content_img))
+                gen_img_content = getattr(self.vgg, self.content_layer)
+                content_img_content = sess.run(gen_img_content)
+            self._content_loss(content_img_content, gen_img_content)
 
-        l_content = self._content_loss(content_out, combination_out)
+            # Style loss
+            with tf.compat.v1.Session() as sess:
+                sess.run(self.input_img.assign(self.style_img))
+                style_layers = sess.run([getattr(self.vgg, layer) for layer in self.style_layers])                              
+            self._style_loss(style_layers)
 
-        # Style loss
-        style_maps = []
-        combination_maps = []
-        for layer in self.style_layers:
-            layer_features = self.layer_outputs[layer]
-            style_feature = layer_features[1, :, :, :]
-            style_maps.append(style_feature)
-            combination_feature = layer_features[2, :, :, :]
-            combination_maps.append(combination_feature)
-        style_maps = np.asarray(style_maps)
-        combination_maps = np.asarray(combination_maps)   
-        l_style = self._style_loss(combination_maps, style_maps)
-
-        # Total loss
-        self.total_loss = self.alpha * l_content + self.beta * l_style
-    
-#     def grad(self, img):
-#          grads = K.gradients(self.total_loss, img)
-#          if len(grads) == 1:
-#              grads = grads.flatten().astype('float64')
-#          else:
-#              grads = np.array(grads).flatten().astype('float64')
-#          self.grads = grads
-
-# class Evaluator(object):
-#      def __init__(self, model):
-#          self.loss_value = None
-#          # self.grads_values = None
-#          self.model = model
-
-#      def loss(self, img):
-#          assert self.loss_value is None
-#          self.model.loss(img)
-#          self.loss_value = self.model.total_loss
-#          return self.model.total_loss
-
-#      def grads(self, img):
-#          assert self.loss_value is not None
-#          self.model.grad(img)
-#          self.loss_value = None
-#          # self.grads_values = None
-#          return self.model.grads
+            # Total loss; update to self.total_loss for future optimization
+            self.total_loss = self.content_w * self.content_loss + self.style_w * self.style_loss
 
     def optimize(self):
         self.optimizer = train.AdamOptimizer(self.lr).minimize(self.total_loss, global_step=self.gstep)
 
-    def train(self, n_iters):
-        print("Training starts.")
-        with Session() as sess:
-            sess.run(global_variables_initializer())
-            sess.run(self.input.assign(self.initial_img))
+    def build(self):
+        self.create_input()
+        self.load_vgg()
+        self.loss()
+        self.optimize()
+    
+    def train(self, n_iters=50):
+        with tf.compat.v1.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            sess.run(self.input_img.assign(self.initial_img))
 
             initial_step = self.gstep.eval()
 
             for epoch in range(initial_step, n_iters):
-                print("Current epoch: ", (epoch + 1))
-                print("Current loss: ", self.total_loss.eval())
+                
                 sess.run(self.optimizer)
-                #print(self.input)
                 if epoch == (n_iters - 1):
-                    gen_img = sess.run([self.input])
-                    gen_img = np.asarray(gen_img)
-                    final_img = self._deprocess_img(gen_img)
-                    self.result = final_img
+                    gen_image, total_loss = sess.run([self.input_img, self.total_loss])
+                    # Inverse mean-centering
+                    gen_image += self.vgg.mean_pixels 
+
+                    print("Epoch: ", (epoch + 1))
+                    print("Loss: ", total_loss)
+
                     filepath = "./frames/frame_%d.png" % self.frame_idx
-                    save_img(filepath, final_img)
+                    save_image(filepath, gen_image)
+
 
 if __name__ == "__main__":
-    os.environ['KMP_DUPLICATE_LIB_OK']='True'
-    tf.compat.v1.disable_v2_behavior()
-
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', required=True, help='path to the input')
     parser.add_argument('-s', '--style_img', required=True, help='path to the style image')
     # parser.add_argument('-o', '--output', required=True, help='output path')
     parser.add_argument('--width', required=False, default=400, type=int, help='output image width')
     parser.add_argument('--height', required=False, default=300, type=int, help='output image height')
-    parser.add_argument('--lr', required=False, default=0.001, type=float, help='hyperparameter: learning rate')
+    parser.add_argument('--lr', required=False, default=2., type=float, help='hyperparameter: learning rate')
     # parser.add_argument('--iter', required=False, default=250, type=int, help='hyperparameter: number of training iterations')
 
     args = parser.parse_args()
@@ -218,9 +163,6 @@ if __name__ == "__main__":
     img_width = args.width
     lr = args.lr
 
-    content = load_img(input_path, target_size=(img_height, img_width))
-    content = img_to_array(content)
-    content = np.expand_dims(content, 0)
-    model = Model(content, style_path, img_height, img_width, lr, 0)
-    model.optimize()
+    model = Image(input_path, style_path, img_height, img_width, lr, 0)
+    model.build()
     model.train(10)
