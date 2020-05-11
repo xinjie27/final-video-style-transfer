6d512544ff4e625560b263b8874d90c8e7c167c6
xinjie27 <45434503+xinjie27@users.noreply.github.com>
2020-05-11 11:26:43 -0400

    removed chinese

--- utils.py
+++ utils.py
@@ -9,14 +9,9 @@ import imageio
 
 def download(download_link, file_name):
     """
-    下载pre-trained VGG-19
-    
-    :param download_link: 下载链接
-    :param file_name: 文件名
-    :param expected_bytes: 文件大小
+    Download a file given its URL
     """
     if os.path.exists(file_name):
-        print("VGG-19 pre-trained model is ready")
         return
     print("Downloading the VGG pre-trained model. This might take a while ...")
     file_name, _ = urllib.request.urlretrieve(download_link, file_name)
@@ -26,16 +21,10 @@ def download(download_link, file_name):
 
 def get_resized_image(img_path, width, height):
     """
-    对图片进行像素尺寸的规范化
-    
-    :param img_path: 图像路径
-    :param width: 像素宽度
-    :param height: 像素高度
-    :param save: 存储路径
-    :return: 
+    Load and resize an image into the desired height and width
     """
     image = Image.open(img_path)
-    # PIL is column major so you have to swap the places of width and height
+    # PIL is column major
     image = ImageOps.fit(image, (width, height), Image.ANTIALIAS)
 
     image = np.asarray(image, np.float32)
@@ -44,19 +33,13 @@ def get_resized_image(img_path, width, height):
 
 def generate_noise_image(content_image, width, height, noise_ratio=0.6):
     """
-    对原图片增加白噪声
-    
-    :param content_image: 内容图片
-    :param width: 图片width
-    :param height: 图片height
-    :param noise_ratio: 噪声比例
-    :return: 带有噪声的内容图片
+    Randonly generate noise in an image
     """
     noise_image = np.random.uniform(-20, 20, (1, height, width, 3)).astype(np.float32)
     return noise_image * noise_ratio + content_image * (1 - noise_ratio)
 
 
 def save_image(path, image):
-    image = image[0]
+    image = image[0] # tensor to image
     image = np.clip(image, 0, 255).astype('uint8')
     imageio.imwrite(path, image)
